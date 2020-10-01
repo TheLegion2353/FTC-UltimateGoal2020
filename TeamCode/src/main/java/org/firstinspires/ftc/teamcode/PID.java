e28cd198adaf0d23c10decac2c700c05210959cf
Geeoon Chung <geeoon13@gmail.com>
2020-10-01 16:52:08 -0500

    Updated PID class.  Need to start work on the loop itself.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class PID {
+    private double Pval = 0;
+    private double Ival = 0;
+    private double Dval = 0;
+    private double setPoint = 0;
+
+    public PID(DcMotor m, double P, double I, double D) {
+        Pval = P;
+        Ival = I;
+        Dval = D;
+    }
+
+    public double PIDLoop(double currentPos) {
+        double processVar = 0;
+        return processVar;
+    }
+
+    public void setSetPoint(double sp) {
+        setPoint = sp;
+    }
 }

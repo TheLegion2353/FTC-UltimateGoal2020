67c982154602e44c27ca2310ac2fa99825238b44
Geeoon Chung <geeoon13@gmail.com>
2020-10-01 16:53:51 -0500

    Removed references to motor in PID class.  Added updateConst function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -1,16 +1,15 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class PID {
-    private double Pval = 0;
-    private double Ival = 0;
-    private double Dval = 0;
+    private double kP = 0;
+    private double kI = 0;
+    private double kD = 0;
     private double setPoint = 0;
 
-    public PID(DcMotor m, double P, double I, double D) {
-        Pval = P;
-        Ival = I;
-        Dval = D;
+    public PID(double P, double I, double D) {
+        kP = P;
+        kI = I;
+        kD = D;
     }
 
     public double PIDLoop(double currentPos) {
@@ -20,5 +19,11 @@ public class PID {
 
     public void setSetPoint(double sp) {
         setPoint = sp;
+    }
+
+    public void updateConst(double P, double I, double D) {
+        kP = P;
+        kI = I;
+        kD = D;
     }
 }

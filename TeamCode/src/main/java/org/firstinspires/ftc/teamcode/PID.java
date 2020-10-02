8f16de194b7bd26c8447cb67148277314dd02afb
Geeoon Chung <geeoon13@gmail.com>
2020-10-02 09:41:48 -0500

    Added errorOverTime and errorOverTimeMax for PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -4,8 +4,11 @@ public class PID {
     private double kP = 0;
     private double kI = 0;
     private double kD = 0;
+    private double errorOverTimeMax = 10;
     private double setPoint = 0;
-
+    private double error = 0;
+    private double errorOverTime = 0;
+    
     public PID(double P, double I, double D) {
         kP = P;
         kI = I;
@@ -14,6 +17,11 @@ public class PID {
 
     public double PIDLoop(double currentPos, double elapsedTime) {
         double processVar = 0;
+        error = setPoint - currentPos;
+        errorOverTime += error * elapsedTime;
+        if (errorOverTime > errorOverTimeMax) {
+            errorOverTime = errorOverTimeMax;
+        }
         return processVar;
     }
 
@@ -28,14 +36,14 @@ public class PID {
     }
     
     private double calcP(double currentPos) {
-        return (setPoint - currrentPos) * kP;
+        return error * kP;
     }
     
-    private double calcI(double currentPos, double elapsedTime) {
-        return (setPoint - currrentPos) * kP;
+    private double calcI(double currentPos) {
+        return errorOverTime * kI;
     }
     
     private double calcD(double currentPos, double elapsedTime) {
-        return (setPoint - currrentPos) * kP;
+        return ((setPoint - currentPos)/elapsedTime) * kD;
     }
 }
